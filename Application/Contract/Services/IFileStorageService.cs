3eadb97d573d90ee9a74c5c67594b6926fa0a930
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-09-19 11:42:49 +0300

    Added Some services
    - Added ICacheService.
    - Added IFileStorageService.
    - Added IUnitOfWork.

---
+++ Application/Contract/Services/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.Contract.Services;
+
+public interface IFileStorageService
+{
+    Task<string> UploadFileAsync(IFormFile file, string category, CancellationToken cancellationToken = default);
+    Task<bool> DeleteFileAsync(string fileUrl, CancellationToken cancellationToken = default);
+}
