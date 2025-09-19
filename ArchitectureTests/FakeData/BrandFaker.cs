0dcbeb29764b602682ebd6fbc44c639e34bca446
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-09-19 11:16:45 +0300

    Added ArchitectureTests project
    - Added Architecture layerd test.
    - Added Bogus with fake data classes.

---
+++ ArchitectureTests/FakeData/BrandFaker.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using Domain.Entities;
+
+namespace ArchitectureTests.FakeData;
+
+public class BrandFaker : Faker<Brand>
+{
+    public BrandFaker()
+    {
+        RuleFor(b => b.Id, f => Guid.CreateVersion7());
+        RuleFor(b => b.Name, f => $"{f.Company.CompanyName()}-{f.UniqueIndex}");
+        RuleFor(b => b.Image, f => $"{f.Image.PicsumUrl()}/{f.UniqueIndex}");
+    }
+}
