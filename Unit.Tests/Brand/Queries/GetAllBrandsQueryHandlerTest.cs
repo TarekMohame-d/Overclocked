3d43cc13320e3faaf26d1c53e263392a4863f728
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-09-19 11:24:34 +0300

    Added Brand Queries Unit Tests
    - Added GetBrandById unit tests.
    - Added GetAllBrands unit test.

---
+++ Unit.Tests/Brand/Queries/GetAllBrandsQueryHandlerTest.cs
@@ -0,0 +1,72 @@
+using System.Net;
+using Application.Features.Brand.Mapping;
+using Application.Features.Brand.Queries.GetAllBrands;
+using ArchitectureTests.FakeData;
+using Domain.Repositories;
+using NSubstitute;
+using Shouldly;
+
+namespace Unit.Tests.Brand.Queries;
+
+public class GetAllBrandsQueryHandlerTest
+{
+    private readonly IBrandRepository _brandRepositoryMock;
+    private readonly GetAllBrandsQueryHandler _handler;
+
+    public GetAllBrandsQueryHandlerTest()
+    {
+        _brandRepositoryMock = Substitute.For<IBrandRepository>();
+        _handler = new GetAllBrandsQueryHandler(_brandRepositoryMock);
+    }
+
+    [Fact]
+    public async Task Handle_WhenBrandsExist_ShouldReturnBrands()
+    {
+        // Arrange
+        var query = new GetAllBrandsQuery();
+        var brands = new BrandFaker().Generate(3);
+
+        var brandDtos = brands.ToDto();
+
+        _brandRepositoryMock.GetAllAsync(Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns(brands);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Data.ShouldNotBeNull();
+        result.Error.ShouldBeNull();
+        brands.Count.ShouldBe(result.Data.Count());
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+        brandDtos.ShouldBeEquivalentTo(result.Data);
+
+        await _brandRepositoryMock.Received(1)
+            .GetAllAsync(Arg.Any<bool>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task Handle_WhenBrandsDoesNotExist_ShouldReturnEmptyList()
+    {
+        // Arrange
+        var query = new GetAllBrandsQuery();
+
+        _brandRepositoryMock.GetAllAsync(Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns([]);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Data.ShouldNotBeNull();
+        result.Error.ShouldBeNull();
+        result.Data.ShouldBeEmpty();
+        result.Data.Count().ShouldBe(0);
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await _brandRepositoryMock.Received(1)
+            .GetAllAsync(Arg.Any<bool>(), Arg.Any<CancellationToken>());
+    }
+}
---
+++ Unit.Tests/Brand/Queries/GetBrandByIdQueryHandlerTests.cs
@@ -0,0 +1,75 @@
+using System.Net;
+using Application.Common.Results;
+using Application.Contract.Services;
+using Application.Features.Brand.Mapping;
+using Application.Features.Brand.Queries.GetBrandById;
+using ArchitectureTests.FakeData;
+using Domain.Repositories;
+using NSubstitute;
+using Shouldly;
+using BrandEntity = Domain.Entities.Brand;
+
+namespace Unit.Tests.Brand.Queries;
+
+public class GetBrandByIdQueryHandlerTests
+{
+    private readonly IBrandRepository _brandRepositoryMock;
+    private readonly GetBrandByIdQueryHandler _handler;
+
+    public GetBrandByIdQueryHandlerTests()
+    {
+        _brandRepositoryMock = Substitute.For<IBrandRepository>();
+        _handler = new GetBrandByIdQueryHandler(_brandRepositoryMock);
+    }
+
+    [Fact]
+    public async Task Handle_WhenBrandExists_ShouldReturnBrand()
+    {
+        // Arrange
+        var brandId = Guid.CreateVersion7();
+        var query = new GetBrandByIdQuery { Id = brandId };
+        var brand = new BrandFaker().Generate();
+        var brandDto = brand.ToDto();
+
+        _brandRepositoryMock.GetByIdAsync(Arg.Any<object[]>(), Arg.Any<CancellationToken>())
+            .Returns(brand);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+        result.Data.ShouldNotBeNull();
+        brandDto.ShouldBeEquivalentTo(result.Data);
+
+        await _brandRepositoryMock.Received(1)
+            .GetByIdAsync(Arg.Any<object[]>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task Handle_WhenBrandDoesNotExists_ShouldReturnFailure()
+    {
+        // Arrange
+        var brandId = Guid.CreateVersion7();
+        var query = new GetBrandByIdQuery { Id = brandId };
+
+        _brandRepositoryMock.GetByIdAsync(Arg.Any<object[]>(), Arg.Any<CancellationToken>())
+            .Returns((BrandEntity)null!);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.ShouldBeFalse();
+        result.Data.ShouldBeNull();
+        result.Error.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        result.Error.Type.ShouldBe(ErrorType.NotFound);
+
+        await _brandRepositoryMock.Received(1)
+            .GetByIdAsync(Arg.Any<object[]>(), Arg.Any<CancellationToken>());
+    }
+}
