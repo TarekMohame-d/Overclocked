5976283af75c4dae172acc423375cf903d3fbfe8
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-09-22 13:37:52 +0300

    Added Category & Brand Controllers.
    - Added Api project with its configurations.

---
+++ Api/Routing/BaseRoute.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Common.Routing;
+
+public class BaseRoute
+{
+    public const string Root = "api";
+    public const string Version = "v1";
+    public const string Base = $"{Root}/{Version}";
+}
---
+++ Api/Routing/CategoryRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Common.Routing;
+
+public class CategoryRoutes : BaseRoute
+{
+    public const string Prefix = $"{Base}/category";
+
+    public const string GetById = $"{Prefix}/{{id:guid}}";
+    public const string GetAll = $"{Base}/categories";
+    public const string Create = $"{Prefix}";
+    public const string Update = $"{Prefix}/{{id:guid}}";
+    public const string Delete = $"{Prefix}/{{id:guid}}";
+}
