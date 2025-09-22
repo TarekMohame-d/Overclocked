63a8445903878593e7c288479503940a82e2a5c0
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-09-22 10:03:47 +0300

    Added Brand Integration Tests.

---
+++ Integration.Tests/BrandTests/Queries/GetByIdBrandTests.cs
@@ -0,0 +1,187 @@
+﻿using Api.Common.Routing;
+using Application.Abstraction.Services;
+using Application.Common.Constants;
+using Application.Common.Results;
+using Application.Features.Brand.Mapping;
+using Application.Features.Brand.Queries.GetBrandById;
+using ArchitectureTests.FakeData;
+using Infrastructure.Data;
+using Integration.Tests.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System.Net;
+using System.Net.Http.Json;
+using Domain.Entities;
+
+namespace Integration.Tests.BrandTests.Queries;
+
+[Collection(nameof(SharedTestCollection))]
+public class GetByIdBrandTests : IAsyncLifetime
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+
+    public GetByIdBrandTests(CustomWebApplicationFactory factory)
+    {
+        _client = factory.HttpClient;
+        _factory = factory;
+    }
+
+    public async Task InitializeAsync() => await _factory.ResetDatabaseAsync();
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    [Fact]
+    public async Task GetById_WhenNotFound_ShouldReturnFailure()
+    {
+        // Arrange
+        Guid id = Guid.CreateVersion7();
+
+        // Act
+        var response = await _client.GetAsync(BrandRoutes.GetById.Replace("{id:guid}", id.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        var result = await response.Content.ReadFromJsonAsync<Result<BrandDto>>();
+
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeFalse();
+        result.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        result.Data.ShouldBeNull();
+        result.Error.ShouldNotBeNull();
+        result.Error.Type.ShouldBe(ErrorType.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_WhenIdIsMalformedGuid_ShouldReturnNotFound()
+    {
+        // Arrange
+        var wrongId = "abc";
+
+        // Act
+        var response = await _client.GetAsync(BrandRoutes.GetById.Replace("{id:guid}", wrongId.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_WhenCacheMiss_ShouldReturnBrandFromDatabase()
+    {
+        // Arrange
+        var brand = await SeedDatabaseAsync();
+
+        // Act
+        var response = await _client.GetAsync(BrandRoutes.GetById.Replace("{id:guid}", brand.Id.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var result = await response.Content.ReadFromJsonAsync<Result<BrandDto>>();
+
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+        result.Data.ShouldNotBeNull();
+        result.Data.Id.ShouldBe(brand.Id);
+        result.Data.Name.ShouldBe(brand.Name);
+    }
+
+    [Fact]
+    public async Task GetById_WhenCacheHit_ShouldReturnBrandFromCache()
+    {
+        // Arrange
+        var brandDto = await SeedCacheAsync();
+
+        // Act
+        var response = await _client.GetAsync(BrandRoutes.GetById.Replace("{id:guid}", brandDto.Id.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var result = await response.Content.ReadFromJsonAsync<Result<BrandDto>>();
+
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+        result.Data.ShouldNotBeNull();
+        result.Data.Name.ShouldBe(brandDto.Name);
+        result.Data.ImageUrl.ShouldBe(brandDto.ImageUrl);
+    }
+
+    [Fact]
+    public async Task GetById_WhenCalledConcurrently_ShouldReturnConsistentResults()
+    {
+        // Arrange
+        var brands = await SeedDatabaseRangeAsync(10);
+        var ids = brands.Select(x => x.Id).ToList();
+        int concurrentCalls = 10;
+        var rnd = new Random();
+        var tasks = new List<Task<HttpResponseMessage>>();
+
+        // Act
+        for (int i = 0; i < concurrentCalls; i++)
+        {
+            var randomId = ids[rnd.Next(ids.Count)];
+            var task = _client.GetAsync(BrandRoutes.GetById.Replace("{id:guid}", randomId.ToString()));
+            tasks.Add(task);
+        }
+
+        await Task.WhenAll(tasks);
+
+        // Assert
+        foreach (var task in tasks)
+        {
+            var response = await task;
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+            var result = await response.Content.ReadFromJsonAsync<Result<BrandDto>>();
+
+            result.ShouldNotBeNull();
+            result.IsSuccess.ShouldBeTrue();
+            result.StatusCode.ShouldBe(HttpStatusCode.OK);
+            result.Data.ShouldNotBeNull();
+        }
+    }
+
+    private async Task<Brand> SeedDatabaseAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+
+        var brand = new BrandFaker().Generate();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Brands.Add(brand);
+        await dbContext.SaveChangesAsync();
+
+        return brand;
+    }
+
+    private async Task<IEnumerable<Brand>> SeedDatabaseRangeAsync(int count = 10)
+    {
+        using var scope = _factory.Services.CreateScope();
+
+        var brands = new BrandFaker().Generate(count);
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Brands.AddRangeAsync(brands);
+        await dbContext.SaveChangesAsync();
+
+        return brands;
+    }
+
+    private async Task<BrandDto> SeedCacheAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+
+        var brand = new BrandFaker().Generate();
+
+        var cache = scope.ServiceProvider.GetRequiredService<ICacheService>();
+        var key = CacheKeys.Brand(brand.Id.ToString());
+        var brandDto = brand.ToDto();
+        Result<BrandDto> result = Result<BrandDto>.Success(brandDto);
+        await cache.SetAsync(key, result);
+
+        return brandDto;
+    }
+}
---
+++ Integration.Tests/Shared/CustomWebApplicationFactory.cs
@@ -0,0 +1,154 @@
+﻿using System.Data.Common;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Api;
+using Application.Abstraction.Services;
+using Application.Common.Constants;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using NSubstitute;
+using Respawn;
+using StackExchange.Redis;
+using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
+
+namespace Integration.Tests.Shared;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("OverclockedDBTest")
+        .WithUsername("admin")
+        .WithPassword("admin-pw")
+        .Build();
+
+    private readonly RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("redis:latest")
+        .Build();
+
+    private DbConnection _dbConnection = default!;
+    private Respawner _respawner = default!;
+    public HttpClient HttpClient { get; private set; } = default!;
+
+    public IFileStorageService FileStorageServiceMock { get; } = Substitute.For<IFileStorageService>();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IFileStorageService>();
+
+            services.AddScoped(_ => FileStorageServiceMock);
+        });
+
+        Environment.SetEnvironmentVariable(
+            "ConnectionStrings:DefaultConnection",
+            _dbContainer.GetConnectionString());
+
+        Environment.SetEnvironmentVariable(
+            "ConnectionStrings:Redis",
+            _redisContainer.GetConnectionString());
+
+        Environment.SetEnvironmentVariable("JwtSettings:SigningKey", "6a5080d2a0d8faea6fb1bfabba8919b15ede19a05f785a2552f8bbc04a0ff9beec356bcd");
+        Environment.SetEnvironmentVariable("JwtSettings:Issuer", "TestIssuer");
+        Environment.SetEnvironmentVariable("JwtSettings:Audience", "TestAudience");
+        Environment.SetEnvironmentVariable("JwtSettings:ExpiryMinutes", "60");
+
+
+        //builder.ConfigureServices(services =>
+        //{
+        //    // Remove old DbContext if present
+        //    var descriptor = services.SingleOrDefault(
+        //        d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+        //    if (descriptor is not null)
+        //        services.Remove(descriptor);
+
+        //    // Add new DbContext with test container connection string
+        //    services.AddDbContext<ApplicationDbContext>(options =>
+        //    {
+        //        options.UseNpgsql(_dbContainer.GetConnectionString());
+        //    });
+        //});
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        await _redisContainer.StartAsync();
+
+        _dbConnection = new NpgsqlConnection(_dbContainer.GetConnectionString());
+        await _dbConnection.OpenAsync();
+        await ApplyMigrationsAsync();
+        HttpClient = CreateClient();
+        await InitializeRespawnerAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbConnection.DisposeAsync();
+        await _dbContainer.DisposeAsync();
+        await _redisContainer.DisposeAsync();
+    }
+
+    public async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_dbConnection);
+
+        var redis = ConnectionMultiplexer.Connect(_redisContainer.GetConnectionString());
+        var db = redis.GetDatabase();
+        await db.ExecuteAsync("FLUSHDB");
+    }
+
+    private async Task InitializeRespawnerAsync()
+    {
+        _respawner = await Respawner.CreateAsync(_dbConnection, new RespawnerOptions
+        {
+            SchemasToInclude = ["public"],
+            DbAdapter = DbAdapter.Postgres,
+            WithReseed = true
+        });
+    }
+
+    private async Task ApplyMigrationsAsync()
+    {
+        using var scope = Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+    public string GenerateJwtToken(string userId = "test-user-id", string role = "Admin")
+    {
+        var claims = new List<Claim>
+            {
+                new Claim(ClaimsConstants.NameIdentifier, userId.ToString()),
+                new Claim(ClaimsConstants.Role, role)
+            };
+
+        var creds = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6a5080d2a0d8faea6fb1bfabba8919b15ede19a05f785a2552f8bbc04a0ff9beec356bcd")),
+            SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(60),
+            SigningCredentials = creds,
+            Issuer = "TestIssuer",
+            Audience = "TestAudience"
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var accessToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(accessToken);
+    }
+}
