0819a0aec28811cfe9b0d9c9e36d50781fa22acc
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-08 20:53:02 +0200

    Added Login Unit Tests

---
+++ Overclocked.Architecture.Tests/FakeData/CategoryFaker.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using Overclocked.Domain.CategoryAggregate;
+using Overclocked.Domain.CategoryAggregate.ValueObjects;
+
+namespace Overclocked.Architecture.Tests.FakeData;
+
+public sealed class CategoryFaker : Faker<Category>
+{
+    public CategoryFaker()
+    {
+        CustomInstantiator(f =>
+            Category.Create(
+                CategoryId.Create(),
+                $"{f.Company.CompanyName()}-{f.UniqueIndex}",
+                $"https://res.cloudinary.com/over-clocked/categories/image.jpg"
+            )
+        );
+    }
+}
---
+++ Overclocked.Architecture.Tests/FakeData/UserFaker.cs
@@ -0,0 +1,35 @@
+using Bogus;
+using Overclocked.Application.Abstraction.Services;
+using Overclocked.Domain.Common.StaticData;
+using Overclocked.Domain.RoleAggregate.ValueObjects;
+using Overclocked.Domain.UserAggregate;
+using Overclocked.Domain.UserAggregate.ValueObjects;
+
+namespace Overclocked.Architecture.Tests.FakeData;
+
+public sealed class UserFaker : Faker<User>
+{
+    private readonly IPasswordHasher _passwordHasher;
+    public UserFaker(IPasswordHasher passwordHasher)
+    {
+        _passwordHasher = passwordHasher;
+        CustomInstantiator(f =>
+            User.Create(
+                UserId.Create(),
+                RoleId.Create((int)RoleType.Customer),
+                $"{f.Person.FirstName}-{f.UniqueIndex}",
+                $"{f.Person.LastName}/{f.UniqueIndex}",
+                $"{f.Internet.Email()}",
+                GeneratePasswordHash(),
+                $"{f.Phone.PhoneNumber()}/{f.UniqueIndex}",
+                $"{f.Internet.Port()}/{f.UniqueIndex}",
+                $"{f.Internet.Port()}/{f.UniqueIndex}"
+            ));
+    }
+
+    private string GeneratePasswordHash()
+    {
+        var password = "P@ssword123";
+        return _passwordHasher.Hash(password);
+    }
+}
