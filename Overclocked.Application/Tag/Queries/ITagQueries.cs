e6fb4f8d861a4649529a35b2dd7290da015ec720
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-09 11:44:03 +0200

    Added Tag Queries Feature.

---
+++ Overclocked.Application/Tag/Queries/ITagQueries.cs
@@ -0,0 +1,12 @@
+using Overclocked.Application.Tag.Queries.GetTags;
+using Overclocked.Contracts.Tag;
+using Overclocked.Domain.Common.Results;
+
+namespace Overclocked.Application.Tag.Queries;
+
+public interface ITagQueries
+{
+    Task<Result<PagedResult<TagListResponse>>> GetPagedTagsQueryHandler(
+        GetPagedTagsQuery query,
+        CancellationToken cancellationToken);
+}
