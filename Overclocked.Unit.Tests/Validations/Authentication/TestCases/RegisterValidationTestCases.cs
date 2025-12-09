44028a8ec1673669765c6f619c79ea966d590aa7
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-09 09:24:30 +0200

    Added Register Unit Test.

---
+++ Overclocked.Unit.Tests/Validations/Authentication/TestCases/RegisterValidationTestCases.cs
@@ -0,0 +1,51 @@
+namespace Overclocked.Unit.Tests.Validations.Authentication.TestCases;
+
+public static class RegisterValidationTestCases
+{
+    public static IEnumerable<object[]> InvalidEmailCases()
+    {
+        yield return [null!];
+        yield return [string.Empty];
+        yield return ["   "];
+        yield return [new string('a', 105)];
+        yield return ["wrong-email-formate"];
+        yield return ["temp@temp"];
+    }
+
+    public static IEnumerable<object[]> InvalidPasswordCases()
+    {
+        yield return [null!];
+        yield return [string.Empty];
+        yield return ["   "];
+        yield return [new string('a', 7)];
+        yield return ["wrong-password-formate"];
+        yield return ["Test12345"];
+        yield return ["test12345"];
+        yield return ["test12345@"];
+    }
+
+    public static IEnumerable<object[]> InvalidFirstNameCases()
+    {
+        yield return [null!];
+        yield return [string.Empty];
+        yield return ["   "];
+        yield return [new string('a', 21)];
+    }
+
+    public static IEnumerable<object[]> InvalidLastNameCases()
+    {
+        yield return [null!];
+        yield return [string.Empty];
+        yield return ["    "];
+        yield return [new string('a', 21)];
+    }
+
+    public static IEnumerable<object[]> InvalidPhoneNumberCases()
+    {
+        yield return [null!];
+        yield return [string.Empty];
+        yield return ["   "];
+        yield return [new string('a', 21)];
+        yield return ["013515abc125"];
+    }
+}
