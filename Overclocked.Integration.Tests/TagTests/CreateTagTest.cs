3f501018ff2de0951215d7bf3f6d41257d232073
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-09 11:42:13 +0200

    Added Tag Commands Integration Tests.

---
+++ Overclocked.Integration.Tests/TagTests/CreateTagTest.cs
@@ -0,0 +1,109 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Overclocked.Api.Routing;
+using Overclocked.Architecture.Tests.FakeData;
+using Overclocked.Domain.Common.StaticData;
+using Overclocked.Domain.TagAggregate;
+using Overclocked.Infrastructure.Persistence;
+using Overclocked.Integration.Tests.Shared;
+using Shouldly;
+
+namespace Overclocked.Integration.Tests.TagTests;
+
+[Collection(nameof(SharedTestCollection))]
+public class CreateTagTest(CustomWebApplicationFactory factory) : IAsyncLifetime
+{
+    private readonly HttpClient _client = factory.HttpClient;
+
+    public async Task InitializeAsync()
+    {
+        await factory.ResetDatabaseAsync();
+
+        var token = CustomWebApplicationFactory.GenerateJwtToken(
+            permissions: [PermissionType.AddEditDelete.ToString()]);
+        factory.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    [Fact]
+    public async Task Create_Should_CreateAndReturnSuccess_When_DataIsValid()
+    {
+        // Arrange
+        const string Name = "AMD";
+
+        StringContent form = CreateJsonContent(Name);
+
+        // Act
+        HttpResponseMessage response = await _client.PostAsync(TagRoutes.Create, form);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+        using IServiceScope scope = factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        Tag? tag = await dbContext.Tags.SingleOrDefaultAsync(x => x.NormalizedName == Name.ToUpper());
+        tag.ShouldNotBeNull();
+
+        tag.Name.ShouldBe(Name);
+    }
+
+    [Fact]
+    public async Task Create_Should_ReturnBadRequest_When_NameAlreadyExists()
+    {
+        // Arrange
+        Tag tag = await SeedDatabaseAsync();
+        StringContent form = CreateJsonContent(tag.Name);
+
+        // Act
+        HttpResponseMessage response = await _client.PostAsync(TagRoutes.Create, form);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Create_Should_ReturnForbidden_When_UserDoesNotHavePermission()
+    {
+        // Arrange
+        Tag tag = await SeedDatabaseAsync();
+        StringContent form = CreateJsonContent(tag.Name);
+
+        var token = CustomWebApplicationFactory.GenerateJwtToken();
+        factory.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        HttpResponseMessage response = await _client.PostAsync(TagRoutes.Create, form);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+    }
+
+    private async Task<Tag> SeedDatabaseAsync()
+    {
+        Tag tag = new TagFaker().Generate();
+
+        using IServiceScope scope = factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Tags.Add(tag);
+        await dbContext.SaveChangesAsync();
+
+        return tag;
+    }
+
+    private static StringContent CreateJsonContent(string name)
+    {
+        var payload = new { Name = name };
+
+        var json = JsonSerializer.Serialize(payload);
+
+        return new StringContent(json, Encoding.UTF8, "application/json");
+    }
+}
