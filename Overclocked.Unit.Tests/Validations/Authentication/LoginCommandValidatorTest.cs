b1ade16b0bbeda48a81bc8232b73d0128d5a3bb2
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-09 09:25:10 +0200

    Added Register And Login Validation Tests.

---
+++ Overclocked.Unit.Tests/Validations/Authentication/LoginCommandValidatorTest.cs
@@ -0,0 +1,60 @@
+using FluentValidation.TestHelper;
+using Overclocked.Application.Authentication.Commands.Login;
+using Overclocked.Unit.Tests.Validations.Authentication.TestCases;
+using Shouldly;
+
+namespace Overclocked.Unit.Tests.Validations.Authentication;
+
+public class LoginCommandValidatorTest
+{
+    [Theory]
+    [MemberData(nameof(LoginValidationTestCases.InvalidEmailCases), MemberType = typeof(LoginValidationTestCases))]
+    public void LoginCommandValidator_Should_ReturnError_When_EmailIsInvalid(string? email)
+    {
+        // Arrange
+        var validator = new LoginCommandValidator();
+        var request = new LoginCommand(email!, "P@ssword1", "device-id");
+
+        // Act
+        TestValidationResult<LoginCommand> result = validator.TestValidate(request);
+
+        // Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldNotBeEmpty();
+        result.ShouldHaveValidationErrorFor(x => x.Email).Only();
+    }
+
+    [Theory]
+    [MemberData(nameof(LoginValidationTestCases.InvalidPasswordCases), MemberType = typeof(LoginValidationTestCases))]
+    public void LoginCommandValidator_Should_ReturnError_When_PasswordIsInvalid(string? password)
+    {
+        // Arrange
+        var validator = new LoginCommandValidator();
+        var request = new LoginCommand("email@gmail.com", password!, "device-id");
+
+        // Act
+        TestValidationResult<LoginCommand> result = validator.TestValidate(request);
+
+        // Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldNotBeEmpty();
+        result.ShouldHaveValidationErrorFor(x => x.Password).Only();
+    }
+
+    [Theory]
+    [MemberData(nameof(LoginValidationTestCases.InvalidDeviceIdCases), MemberType = typeof(LoginValidationTestCases))]
+    public void LoginCommandValidator_Should_ReturnError_When_DeviceIdIsInvalid(string? deviceId)
+    {
+        // Arrange
+        var validator = new LoginCommandValidator();
+        var request = new LoginCommand("email@gmail.com", "P@ssword1", deviceId!);
+
+        // Act
+        TestValidationResult<LoginCommand> result = validator.TestValidate(request);
+
+        // Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldNotBeEmpty();
+        result.ShouldHaveValidationErrorFor(x => x.DeviceId).Only();
+    }
+}
