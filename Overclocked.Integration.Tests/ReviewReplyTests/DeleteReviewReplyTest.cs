19b4bac77b2dc4b5462b337b8d5eb3835ecbe93d
Tarek Mohamed <tarekmohamed1019@gmail.com>
2025-12-15 12:32:44 +0200

    Added Review Reply Integration Tests.

---
+++ Overclocked.Integration.Tests/ReviewReplyTests/DeleteReviewReplyTest.cs
@@ -0,0 +1,120 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Overclocked.Api.Routing;
+using Overclocked.Architecture.Tests.FakeData;
+using Overclocked.Domain.BrandAggregate;
+using Overclocked.Domain.CategoryAggregate;
+using Overclocked.Domain.Common.StaticData;
+using Overclocked.Domain.ProductAggregate;
+using Overclocked.Domain.ReviewAggregate;
+using Overclocked.Domain.ReviewAggregate.Entities;
+using Overclocked.Domain.ReviewAggregate.ValueObjects;
+using Overclocked.Domain.UserAggregate;
+using Overclocked.Infrastructure.Authentication;
+using Overclocked.Infrastructure.Persistence;
+using Overclocked.Integration.Tests.Shared;
+using Shouldly;
+
+namespace Overclocked.Integration.Tests.ReviewReplyTests;
+
+[Collection(nameof(SharedTestCollection))]
+public class DeleteReviewReplyTest(CustomWebApplicationFactory factory) : IAsyncLifetime
+{
+    private readonly HttpClient _client = factory.HttpClient;
+
+    public async Task InitializeAsync()
+    {
+        await factory.ResetDatabaseAsync();
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    [Fact]
+    public async Task Delete_Should_DeleteAndReturnSuccess_When_DataIsValid()
+    {
+        // Arrange
+        (User user, User admin, User admin2, Product product, Review review) = await SeedDatabaseAsync();
+
+        var token = CustomWebApplicationFactory.GenerateJwtToken(userId: admin.Id.Value.ToString(), role: "Admin");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        HttpResponseMessage response = await _client
+            .DeleteAsync(ReviewReplyRoutes.Delete.Replace("{productId:guid}", product.Id.Value.ToString()).Replace("{reviewId:guid}", review.Id.Value.ToString()).Replace("{replyId:guid}", review.ReviewReply!.Id.Value.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        using IServiceScope scope = factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        Review? reviewDb = await dbContext.Reviews.FirstOrDefaultAsync(x => x.UserId == user.Id);
+
+        reviewDb.ShouldNotBeNull();
+        reviewDb.ProductId.ShouldBe(product.Id);
+        reviewDb.ReviewReply.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Update_Should_ReturnFailure_When_Unauthorized()
+    {
+        // Arrange
+        (User user, User admin, User admin2, Product product, Review review) = await SeedDatabaseAsync();
+
+        var token = CustomWebApplicationFactory.GenerateJwtToken(userId: admin2.Id.Value.ToString(), role: "Admin");
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        HttpResponseMessage response = await _client
+            .DeleteAsync(ReviewReplyRoutes.Delete.Replace("{productId:guid}", product.Id.Value.ToString()).Replace("{reviewId:guid}", review.Id.Value.ToString()).Replace("{replyId:guid}", review.ReviewReply!.Id.Value.ToString()));
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        using IServiceScope scope = factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        Review? reviewDb = await dbContext.Reviews.SingleOrDefaultAsync(x => x.UserId == user.Id);
+
+        reviewDb.ShouldNotBeNull();
+        review.ProductId.ShouldBe(product.Id);
+        reviewDb.ReviewReply.ShouldNotBeNull();
+        reviewDb.ReviewReply.Reply.ShouldBe("Reply");
+        reviewDb.ReviewReply.EmployeeId.ShouldBe(admin.Id);
+    }
+
+    private async Task<(User user, User admin, User admin2, Product product, Review review)> SeedDatabaseAsync()
+    {
+        User user = new UserFaker(new PasswordHasher()).Generate();
+        Brand brand = new BrandFaker().Generate();
+        Category category = new CategoryFaker().Generate();
+        Product product = new ProductFaker(brand.Id.Value, category.Id.Value).Generate();
+        Review review = new ReviewFaker(user.Id.Value, product.Id.Value).Generate();
+
+        User admin = new UserFaker(new PasswordHasher()).Generate();
+        admin.ChangeRole(RoleType.Admin);
+
+        review.AddReviewReply(ReviewReply.Create(ReviewReplyId.Create(), admin.Id, "Reply"));
+
+        User admin2 = new UserFaker(new PasswordHasher()).Generate();
+        admin2.ChangeRole(RoleType.Admin);
+
+        using IServiceScope scope = factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Brands.Add(brand);
+        dbContext.Categories.Add(category);
+        dbContext.Products.Add(product);
+        dbContext.Users.Add(user);
+        dbContext.Users.Add(admin);
+        dbContext.Users.Add(admin2);
+        dbContext.Reviews.Add(review);
+        await dbContext.SaveChangesAsync();
+
+        return (user, admin, admin2, product, review);
+    }
+}
